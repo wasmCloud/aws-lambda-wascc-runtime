afba71fe14d413d613876368d005b05cd59b15aa
Kit Ewbank <Christopher.Ewbank@capitalone.com>
2020-02-26 15:42:06 -0500

    Simplest lambda handler.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use lambda_runtime_core::{lambda, Context, HandlerError};
+use log::{info, Level};
+use simple_logger;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    simple_logger::init_with_level(Level::Debug)?;
+
+    lambda!(wascc_handler);
+
+    Ok(())
+}
+
+fn wascc_handler(_data: Vec<u8>, _ctx: Context) -> Result<Vec<u8>, HandlerError> {
+    info!("wascc_handler entered");
+
+    Ok("all good".as_bytes().to_vec())
 }
