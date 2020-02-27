59c02721a3f808aa32cd52e72e3574a178473df4
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-02-27 18:06:08 -0500

    Initial code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[macro_use]
+extern crate log;
+
+use env_logger;
+
+const CAPABILITY_ID: &str = "awslambda::runtime";
+
+pub struct AwsLambdaRuntimeProvider {}
+
+impl Default for AwsLambdaRuntimeProvider {
+    fn default() -> Self {
+        env_logger::init();
+
+        info!("hello");
+
+        AwsLambdaRuntimeProvider {}
+    }
+}
+
+impl AwsLambdaRuntimeProvider {
+    pub fn new() -> Self {
+        Self::default()
     }
 }
