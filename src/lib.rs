d1a782c9140407ee18524c2da77486782236bb9d
Kit Ewbank <Christopher.Ewbank@capitalone.com>
2020-03-03 16:34:50 -0500

    Implement Into on Event type.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,16 @@
 //
 
 pub mod aws_lambda_runtime {
+    // Include generated code.
     include!(concat!(env!("OUT_DIR"), "/lambda.rs"));
 
+    use prost::Message;
+
     pub const OP_HANDLE_EVENT: &str = "HandleEvent";
+
+    impl Into<Event> for &[u8] {
+        fn into(self) -> Event {
+            Event::decode(self).unwrap()
+        }
+    }
 }
